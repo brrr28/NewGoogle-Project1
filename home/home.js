d007b2f3f4219c60f3c4b992a4af8b405b88856b
LesiaChervinska <zlesia2016@gmail.com>
2021-03-20 12:34:32 +0200

    add info in cards

--- home/home.js
+++ home/home.js
@@ -17,7 +17,7 @@ var card_list_age = document.querySelectorAll('.card_list_age');
 var card_list_color  = document.querySelectorAll('.card_list_color');
 var card_list_phone  = document.querySelectorAll('.card_list_phone');
 var card_list_city = document.querySelectorAll('.card_list_city');
-var card_list_hobbie  =document.querySelectorAll('.card_list_hobbie')
+var card_list_hobbie  = document.querySelectorAll(".card_list_hobbie");
 // ф-ция выводит и перебирает циклом распарсеный JSON
  function showInfoDevelopers(json){
      var dataDevelopInfo = json;
@@ -26,19 +26,23 @@ var card_list_hobbie  =document.querySelectorAll('.card_list_hobbie')
 
 for(var k = 0; k<= card_list_name.length; k++){
     for (var i = 0; i < dataDevelopInfo.developers.length; i++){
+        for(let key in dataDevelopInfo.developers[i]){
+            console.log(key)
         
+        //console.log(Object.keys(dataDevelopInfo.developers[i]))
         //console.log(card_list_hobbie[i]);
-        //console.log(dataDevelopInfo.developers[i].Hobbie[i])
+        //console.log(dataDevelopInfo.developers[i].Hobbie)
 
         card_list_name[i].innerHTML = `Name :${dataDevelopInfo.developers[i].Name}`;
         card_list_age[i].innerHTML = `Age: ${dataDevelopInfo.developers[i].Age}`
         card_list_color[i].innerHTML = `Color: ${dataDevelopInfo.developers[i].Color}`
         card_list_phone[i].innerHTML = `Phone: ${dataDevelopInfo.developers[i].Phone}`
         card_list_city[i].innerHTML = `City: ${dataDevelopInfo.developers[i].City}`
-        card_list_hobbie[i].innerHTML = `Hobbi: ${dataDevelopInfo.developers[i].Hobbie[1]}, </br>${dataDevelopInfo.developers[i].Hobbie[2]}, </br> ${dataDevelopInfo.developers[i].Hobbie[3]}`
+        card_list_hobbie[i].innerHTML = `${dataDevelopInfo.developers[i].Hobbie}`
 
 
     }
+}
 }
 
  }
