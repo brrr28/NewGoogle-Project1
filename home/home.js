cac6ea641809c07688a0e985731f642814352330
LesiaChervinska <zlesia2016@gmail.com>
2021-04-01 14:56:30 +0300

    add derver

--- home/home.js
+++ home/home.js
@@ -1,33 +1,69 @@
-// эмуляция получения фетч запроса через промис
-let infoDevelopers = new Promise(function (resolve, reject) {
-    var a = fetch("infoDevelop.json");
-    resolve(a);
-    reject(new Error("Smsg wrong!"));
-  })
-    .then((response) => response.json())
-    .then(
-      (json) => showInfoDevelopers(json),
-      (error) => console.log("Error")
-    );
-  
-  
-  let card__info = document.getElementsByClassName("card__info"); //html  коллекция для получ всех карточек
-  
-  // ф-ция выводит и перебирает циклом распарсеный JSON и добавляет в карточки инфу
-  function showInfoDevelopers(json) {
-    var dataDevelopInfo = json; // сам распарсеный файл json
-    for (var i = 0; i < card__info.length; i++) {
-      // перебор коллекции
-      for (var i = 0; i <= dataDevelopInfo.developers.length; i++) {
-        var ul = document.createElement("ul");
-        for (let key in dataDevelopInfo.developers[i]) {
-          let li = document.createElement("li");
-          ul.appendChild(li)
-          li.innerHTML = `${[key]}: ${dataDevelopInfo.developers[i][key]}`;
-         
-          card__info[i].appendChild(ul);
-        }
+var apiUrl = "http://localhost:3000";
+function getRequest(url, params) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest(); //
+    xhr.open("GET", apiUrl + url, true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        resolve(xhr.response)
+        //console.log(xhr.responseText);
+      }
+    };
+
+    xhr.send();
+  });
+}
+
+getRequest("/developers")
+.then(function(response) {
+  var data = JSON.parse(response);
+ // console.log( data.developers);
+  showInfoDevelopers(data)
+}).catch(function(error){
+  console.log("Error!!!");
+  console.log(error);
+});
+
+
+
+let card__info = document.getElementsByClassName("card__info"); 
+
+
+function showInfoDevelopers(json) {
+  var dataDevelopInfo = json; 
+  for (var i = 0; i < card__info.length; i++) {
+    for (var i = 0; i <= dataDevelopInfo.developers.length; i++) {
+      var ul = document.createElement("ul");
+      for (let key in dataDevelopInfo.developers[i]) {
+        let li = document.createElement("li");
+        ul.appendChild(li)
+        li.innerHTML = `${[key]}: ${dataDevelopInfo.developers[i][key]}`;
+
+        card__info[i].appendChild(ul);
       }
     }
   }
-  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// var a = {
+//   a: 1,
+
+//postRequest( '/developers', a);
+// function postRequest(url,data){
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", apiUrl + url,false);
+//   xhr.setRequestHeader("Content-Type", "application/json");
+
+//   xhr.send(JSON.stringify(data))
+// }
