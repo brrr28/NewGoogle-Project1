2f8b07bf3abe7305caadc76c2fa7ceaf3bd6c5f2
Bogdan <60dqa1992@gmail.com>
2021-04-02 01:16:54 +0300

    50% modal window finish

--- question/question.js
+++ question/question.js
@@ -1,4 +1,10 @@
-var formButton = document.querySelector('form__addQuastion');
+var formButton = document.querySelector('.selections-box__AddQuestions');
+var popup = document.querySelector('.form__addQuastion');
 formButton.onclick = function () {
-    
+    popup.style.display = 'block';
+};
+
+var closeButton = document.querySelector('.close__button');
+closeButton.onclick = function () {
+    popup.style.display = 'none';
 }
